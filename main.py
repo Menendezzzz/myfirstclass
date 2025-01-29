10cddbfc7eb87f2a7560271680d3cf8ce03995a9
Menendezzzz <marselsarapov53@g,ail.com>
2025-01-29 15:27:43 +0600

    Hallo

---
+++ main.py
@@ -0,0 +1,9 @@
+from car import Car
+
+if __model__ == '__main__':
+
+    car1 = Car('Lexus', 'Black', '2012',price=45000)
+    car1.info()
+
+    car2 = Car('Camry', 'Black', '2009')
+    car2.info()
